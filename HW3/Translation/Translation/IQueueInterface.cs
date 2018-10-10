666594b26bc03612e0f29ee5295be2719bbcf04c
Nicholas Leonard <nleonard17@wou.edu>
2018-10-10 11:53:51 -0700

    Finished XML comments for IQueueInterface

--- HW3/Translation/Translation/IQueueInterface.cs
+++ HW3/Translation/Translation/IQueueInterface.cs
@@ -1,22 +1,30 @@
 ﻿
 namespace Translation
-{
+{/// <summary>
+/// Interface for different queue types.
+/// </summary>
+/// <typeparam name="T"></typeparam>
     public interface IQueueInterface<T>
     {
-        /*Add an element to the rear of the queue and return the element that was enqueued*/
+        /// <summary>
+        /// Add an element to the rear of the queue and return the element that was enqueued.
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
         T Push(T element);
 
-        /*Remove and return the front element
-         throws QueueUnderFlowException if queue is empty*/
            /// <summary>
-           /// 
+           /// Remove and return the front element and throws a QueueUnderFlowException if the queue is empty.
            /// </summary>
            /// <returns></returns>
            
         T Pop();
 
         /*Test if the queue is empty. Return true if empty.*/
-
+        /// <summary>
+        /// Test if the queue is empty. Return true if the queue is empty; false otherwise.
+        /// </summary>
+        /// <returns></returns>
         bool IsEmpty();
     }
 }
