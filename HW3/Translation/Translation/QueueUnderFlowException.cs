ba81ed2086cf3b57a32a409956f2d1fdea15a5e3
Nicholas Leonard <nleonard17@wou.edu>
2018-10-10 12:04:20 -0700

    Finished XML comments on QueueUnderFlowException

--- HW3/Translation/Translation/QueueUnderFlowException.cs
+++ HW3/Translation/Translation/QueueUnderFlowException.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 
 namespace Translation
-{/*A custom exception to represent situations where an illegal operation was performed on an empty queue.*/
-
+{    /// <summary>
+    /// A custom exception to represent situations where an illegal operation was performed on an empty queue.
+    /// </summary>
     public class QueueUnderFlowException : Exception
-    {
+    {//user made exception with no message.
         public QueueUnderFlowException() : base()
         {
         }
 
+        //user made exception that displays a message.
         public QueueUnderFlowException(string message) : base(message)
         {
         }
