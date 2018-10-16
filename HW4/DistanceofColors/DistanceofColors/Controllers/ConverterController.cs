2c58138d30df5f183938da53421febb101a09ee7
Nicholas Leonard <nleonard17@wou.edu>
2018-10-16 11:48:37 -0700

    Simple landing page, with partially working navbar and converter button.

---
+++ HW4/DistanceofColors/DistanceofColors/Controllers/ConverterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DistanceofColors.Controllers
+{
+    public class ConverterController : Controller
+    {
+        // GET: Converter
+        public ActionResult Converter()
+        {
+            return View();
+        }
+    }
+}
--- HW4/DistanceofColors/DistanceofColors/Controllers/HomeController.cs
+++ HW4/DistanceofColors/DistanceofColors/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace DistanceofColors.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Converter()
         {
             ViewBag.Message = "Your application description page.";
 
