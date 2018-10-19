77fe6aefea79bf65aa13aeda15e7e315280044fe
Nicholas Leonard <nleonard17@wou.edu>
2018-10-18 20:51:43 -0700

    Mixed the Colors. Need to get them to render on the page.

--- HW4/DistanceofColors/DistanceofColors/Controllers/ColorController.cs
+++ HW4/DistanceofColors/DistanceofColors/Controllers/ColorController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,70 @@ namespace DistanceofColors.Controllers
         [HttpGet]
         public ActionResult Create()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string FirstColor, string SecondColor)
+        {
+            Debug.WriteLine(FirstColor);
+            Debug.WriteLine(SecondColor);
+            Color PrimaryColor = ColorTranslator.FromHtml(FirstColor);
+            Color SecondaryColor = ColorTranslator.FromHtml(SecondColor);
+
+            //R,B,G int componenets of mixing colors
+            int PrimaryColorR = PrimaryColor.R;
+            int PrimaryColorG = PrimaryColor.G;
+            int PrimaryColorB = PrimaryColor.B;
+            
+            int SecondaryColorR = SecondaryColor.R;
+            int SecondaryColorG = SecondaryColor.G;
+            int SecondaryColorB = SecondaryColor.B;
+
+            int MixedColorR = 0;
+            int MixedColorG = 0;
+            int MixedColorB = 0;
+            
+            //R,B,G int componenets of mixed color
+            if(PrimaryColorR + SecondaryColorR < 255)
+            {
+                MixedColorR = PrimaryColorR + SecondaryColorR;
+            }
+            else
+            {
+               // int MixedColorR = 255;
+            }
+            if(PrimaryColorG + SecondaryColorG < 255)
+            {
+                MixedColorG = PrimaryColorG + SecondaryColorG;
+            }
+            else
+            {
+                MixedColorG = 255;
+            }
+            if(PrimaryColorB + SecondaryColorB < 255)
+            {
+                MixedColorB = PrimaryColorB + SecondaryColorB;
+
+            }
+            else
+            {
+                MixedColorB = 255;
+            }
+            
+            Color MixedColor = Color.FromArgb(MixedColorR, MixedColorG, MixedColorB);
+
+            Debug.WriteLine(PrimaryColor);
+            Debug.WriteLine(SecondaryColor);
+            Debug.WriteLine(MixedColor.R);
+            Debug.WriteLine(MixedColor.G);
+            Debug.WriteLine(MixedColorB);
+
+            string ResultColor = ColorTranslator.ToHtml(MixedColor);
+            Debug.WriteLine(ResultColor);
+
+            ViewBag.MixedColor = ResultColor;
+
             return View();
         }
     }
