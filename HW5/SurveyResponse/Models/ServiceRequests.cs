b5c7922b82fbff45755c3fcd251c8fbedf7dc68c
Nicholas Leonard <nleonard17@wou.edu>
2018-10-24 12:13:50 -0700

    Finished making form page. And added default view for response page.

--- HW5/SurveyResponse/Models/ServiceRequests.cs
+++ HW5/SurveyResponse/Models/ServiceRequests.cs
@@ -8,18 +8,19 @@ namespace SurveyResponse.Models
 {
     public class ServiceRequests
     {
-        [Required, MaxLength(20)]
+        [Required, MaxLength(20), Display(Name = "First Name")]
         public string FirstName { get; set; }
-        [Required, MaxLength(20)]
+        [Required, MaxLength(20), Display(Name = "Last Name")]
         public string LastName { get; set; }
-        [Required, MaxLength(40)]
+        [Required, MaxLength(40), Display(Name = "Apartment Name")]
         public string ApartmentName { get; set; }
-        [Required]
+        [Required, Display(Name = "Building Number")]
         public int UnitNumber { get; set; }
-        [Phone, Required]
+        [Phone, Required, Display(Name = "Phone Number")]
         public string Phone { get; set; }
-        [Required, MaxLength(500)]
+        [Required, MaxLength(500), Display(Name = "Comments")]
         public string Comments { get; set; }
+        [Display(Name = "Approval to Enter")]
         public bool EnterForMaintenance { get; set; }
     }
 }
