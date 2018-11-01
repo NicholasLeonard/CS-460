430b6cdbbe6d571444ab5316933275a3ced266e3
Nicholas Leonard <nleonard17@wou.edu>
2018-11-01 12:30:50 -0700

    Added to the controller with the first query. Modified view. Needs work.

--- HW6/BigDatabase/Controllers/HomeController.cs
+++ HW6/BigDatabase/Controllers/HomeController.cs
@@ -1,22 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.EnterpriseServices;
 using System.Linq;
+using System.Diagnostics;
 using System.Web;
 using System.Web.Mvc;
+using BigDatabase.Controllers;
+using BigDatabase.Models;
+using BigDatabase.Models.ViewModels;
 
 namespace BigDatabase.Controllers
 {
     public class HomeController : Controller
     {
+        private UserContext db = new UserContext();
+
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
+
+            List<PersonVM> SearchResult = db.People.Where(person => person.FullName.Contains("a")).Select(person => new PersonVM { Name = person.FullName, PreferredName = person.PreferredName, PhoneNumber = person.PhoneNumber, FaxNumber = person.FaxNumber, EmailAddress = person.EmailAddress, ValidFrom = person.ValidFrom }).ToList();
+                
             return View();
         }
 
---
+++ HW6/BigDatabase/Models/ViewModels/PersonVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigDatabase.Models.ViewModels
+{
+    public class PersonVM
+    {
+        public string Name { get; set; }
+        public string PreferredName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FaxNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime ValidFrom { get; set; }
+
+        //IEnumerable<Person> People { get; set; }
+    }
+}
