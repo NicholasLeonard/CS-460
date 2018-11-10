cfee161e0e8a453f4c731db7e15e04ddc9290039
Nicholas Leonard <nleonard17@wou.edu>
2018-11-09 18:21:27 -0800

    Got giphs to appear on page.

--- HW7/Giph/Controllers/ApiController.cs
+++ HW7/Giph/Controllers/ApiController.cs
@@ -34,18 +34,23 @@ namespace Giph.Controllers
             //reads the response from the Api
             string ApiResponse = reader.ReadToEnd();
             //parses the json object to get the necessary data and converts it to a string for entry in the next parse
-            string data = JObject.Parse(ApiResponse)["data"].ToString();
+            var data = JObject.Parse(ApiResponse)["data"].ToString();
             //parses the json object to get necessary data and converts to string for entry in next parse
-            string images = JObject.Parse(data)["images"].ToString();
+            //string images = JObject.Parse(data)["images"].ToString();
             //casts the resulting json object to a json object for passing back to the client
-            JObject preview = (JObject)JObject.Parse(images)["preview"];
+            //var preview = JObject.Parse(images)["preview"].ToString();
+            Debug.WriteLine(data);
+            /*var test = new
+            {
+                
+            };*/
             
             //closes connections to Giphy Api
             reader.Close();
             dataStream.Close();
             response.Close();
             
-            return Json(preview, JsonRequestBehavior.AllowGet);
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
     }
 }
