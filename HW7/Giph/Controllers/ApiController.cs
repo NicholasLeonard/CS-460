c5d9f228dbf2f62e86602f3483287a491979e996
Nicholas Leonard <nleonard17@wou.edu>
2018-11-09 16:39:34 -0800

    Got a response from the giphy api and passed it back to the page.

--- HW7/Giph/Controllers/ApiController.cs
+++ HW7/Giph/Controllers/ApiController.cs
@@ -1,26 +1,51 @@
 ﻿using Microsoft.Ajax.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
+using System.IO;
+using System.Text;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
 
 namespace Giph.Controllers
 {
     public class ApiController : Controller
     {
-        // GET: Api
+        [HttpGet]
         public JsonResult Giph(string word)
         {
-            //WebRequest
-            System.Diagnostics.Debug.WriteLine(word);
-            var data = new
-            {
-                message = word
-            };
-            return Json(data, JsonRequestBehavior.AllowGet);
+            //constructs the uri for requesting from the giphy api
+            string key = ConfigurationManager.AppSettings["GiphyAPIKey"];
+            string URL = "https://api.giphy.com/v1/stickers/translate?api_key=" + key + "&s=" + word;
+
+            //creates the connection between the controller and the giphy api
+            WebRequest request = HttpWebRequest.Create(URL);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            
+            //creates a new stream to the giphy api and a new streamreader for that stream
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            //reads the response from the Api
+            string ApiResponse = reader.ReadToEnd();
+            //parses the json object to get the necessary data and converts it to a string for entry in the next parse
+            string data = JObject.Parse(ApiResponse)["data"].ToString();
+            //parses the json object to get necessary data and converts to string for entry in next parse
+            string images = JObject.Parse(data)["images"].ToString();
+            //casts the resulting json object to a json object for passing back to the client
+            JObject preview = (JObject)JObject.Parse(images)["preview"];
+            
+            //closes connections to Giphy Api
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            
+            return Json(preview, JsonRequestBehavior.AllowGet);
         }
     }
 }
