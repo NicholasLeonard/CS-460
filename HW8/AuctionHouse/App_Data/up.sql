c5e8c250ac21b167cb2dec8df73fa3a2702cf3d2
Nicholas Leonard <nleonard17@wou.edu>
2018-11-12 17:43:53 -0800

    Added seed data to up.sql file.

--- HW8/AuctionHouse/App_Data/up.sql
+++ HW8/AuctionHouse/App_Data/up.sql
@@ -29,12 +29,14 @@ CREATE TABLE [dbo].[Bids]
     CONSTRAINT [FK_Bids_Item] FOREIGN KEY ([Item]) REFERENCES [Items]([ID]), 
     CONSTRAINT [FK_Bids_Buyer] FOREIGN KEY ([Buyer]) REFERENCES [Buyers]([BuyerName])	
 );
-/*
+
 INSERT INTO [dbo].[Buyers] (BuyerName) VALUES ('Jane Stone'),('Tom McMasters'),('Otto Vanderwall')
 INSERT INTO [dbo].[Sellers] (SellerName) VALUES ('Gayle Hardy'),('Lyle Banks'),('Pearl Greene')
 INSERT INTO [dbo].[Items] (ItemName, Description, Seller) VALUES
 ('Abraham Lincoln Hammer', 'A bench mallet fashioned from a broken rail-splitting maul in 1829 and owned by Abraham Lincoln', 'Pearl Greene'),
 ('Albert Einsteins Telescope', 'A brass telescope owned by Albert Einstein in Germany, circa 1927', 'Gayle Hardy'),
 ('Bob Dylan Love Poems', 'Five versions of an original unpublished, handwritten, love poem by Bob Dylan', 'Lyle Banks')
-INSERT INTO [dbo].[Bids] (Price, TimeStamp) VALUES (1001,250000,'12/04/2017 09:04:22'), (1003,95000 ,'12/04/2017 08:44:03')
-GO*/
+INSERT INTO [dbo].[Bids] (Item, Buyer, Price, TimeStamp) VALUES
+(0, 'Otto Vanderwall',250000,'12/04/2017 09:04:22'),
+(2,'Jane Stone',95000,'12/04/2017 08:44:03')
+GO
