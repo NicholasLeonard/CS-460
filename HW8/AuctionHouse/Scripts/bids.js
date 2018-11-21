34ef039dce2035e74610c605b1c0146cb0914ace
Nicholas Leonard <nleonard17@wou.edu>
2018-11-21 11:34:09 -0800

    Finished bid display for the details page.

--- HW8/AuctionHouse/Scripts/bids.js
+++ HW8/AuctionHouse/Scripts/bids.js
@@ -6,21 +6,30 @@ var ajax_call = function () {
     var pageURL = window.location.href;
     var id = pageURL.substr(pageURL.lastIndexOf('/') + 1);
     var source = "/Home/Update/" + id;
+    
     $.ajax({
         method: "GET",
         dataType: "json",
         url: source,
         success: displayBids,
-        
+        error: displayError
     });
 };
 
 var interval = 1000 * 5;
 
 function displayBids(AllBids) {
-    console.log(AllBids);
-    console.log("AllBids above");
+
+    $("#bids").empty();
     var test = JSON.parse(AllBids);
-    console.log("test above");
-    console.log(test);
+ 
+    for (var x = 0; x < test.length; x++) {
+        console.log(test[x]);
+        $("#bids").append("<tr id='row'><td>" + test[x].Buyer + "</td > <td>" + test[x].Price + "</td></tr>");
+    }
+}
+
+function displayError() {
+    $("#bids").empty();
+    $("#bids").append("ERROR IN THE AJAX!");
 }
