4c29939eac6cc264e52da8153bc652dc16b7ff7c
Chi Li <cli16@mail.wou.udu>
2019-01-25 10:54:46 -0800

    added homepage for Milestone3

---
+++ Milestone_3/milestone3/milestone3/Models/Topic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace milestone3.Models
+{
+    public class Topic
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        public int Ranking { get; set; }
+
+        public int Views { get; set; }
+
+        [StringLength(50)]
+        public string Category { get; set; }
+
+        //get timestamp
+        public DateTime currentTime = DateTime.Now;
+
+        public DateTime Timestamp
+        {
+            get { return currentTime; }
+            set { currentTime = value; }
+
+        }
+
+    }
+}
