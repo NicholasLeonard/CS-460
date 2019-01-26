de161f77051fc4a357124db1ca10a3d0c6bf641c
Jace Woods <woodsjace@yahoo.com>
2019-01-25 16:22:21 -0800

    topic view now sorts all preseeded topics by descending timestamp

---
+++ Milestone_3/milestone3/milestone3/Controllers/TopicsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using milestone3.Models;
+
+namespace milestone3.Controllers
+{
+    public class TopicsController : Controller
+    {
+        private ToasterItContext db = new ToasterItContext();
+
+        // GET: Topics
+        public ActionResult Index()
+        {
+            var topics = db.Topics.OrderByDescending(t => t.Timestamp);
+            return View(topics.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Milestone_3/milestone3/milestone3/Models/Topic.cs
+++ Milestone_3/milestone3/milestone3/Models/Topic.cs
@@ -1,35 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.ComponentModel.DataAnnotations;
-
 namespace milestone3.Models
 {
-    public class Topic
-    {
-        [Key]
-        public int ID { get; set; }
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
 
+    public partial class Topic
+    {
+        public int TopicId { get; set; }
+
+        [Required]
         [StringLength(50)]
         public string Title { get; set; }
 
-        public int Ranking { get; set; }
+        public int? Ranking { get; set; }
 
         public int Views { get; set; }
 
         [StringLength(50)]
         public string Category { get; set; }
 
-        //get timestamp
-        public DateTime currentTime = DateTime.Now;
-
-        public DateTime Timestamp
-        {
-            get { return currentTime; }
-            set { currentTime = value; }
-
-        }
-
+        [Column(TypeName = "datetime2")]
+        public DateTime Timestamp { get; set; }
     }
 }
