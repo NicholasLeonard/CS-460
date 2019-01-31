8593be37e6e7ebf465e47e02faceb64137f5cdfc
Nicholas Leonard <nleonard17@wou.edu>
2019-01-31 10:38:58 -0800

    Added proper project files with no binaries.

---
+++ Milestone_3/milestone3/milestone3/App_Data/down.sql
@@ -0,0 +1,7 @@
+﻿ALTER TABLE [dbo].[Comments] DROP CONSTRAINT [FK_Comments_Topics]
+ALTER TABLE [dbo].[Topics] DROP CONSTRAINT [FK_Topics_Categories]
+
+DROP TABLE [dbo].[Categories]
+DROP TABLE [dbo].[Topics]
+DROP TABLE [dbo].[Comments]
+GO
