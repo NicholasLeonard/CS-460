cce2081cdf4796bf7c7115e6aa7fc34dbd1edb01
Nicholas Leonard <nleonard17@wou.edu>
2019-01-31 10:37:05 -0800

    Trying to remove binary files from track.

--- Milestone_3/milestone3/milestone3/App_Data/down.sql
+++
@@ -1,7 +0,0 @@
-﻿ALTER TABLE [dbo].[Comments] DROP CONSTRAINT [FK_Comments_Topics]
-ALTER TABLE [dbo].[Topics] DROP CONSTRAINT [FK_Topics_Categories]
-
-DROP TABLE [dbo].[Categories]
-DROP TABLE [dbo].[Topics]
-DROP TABLE [dbo].[Comments]
-GO
