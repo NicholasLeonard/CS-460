884fca12789444bbf078f86029d8bf358d3f4a90
Alex-Bishop1296 <alex.bishop1296@gmail.com>
2019-02-18 16:31:11 -0800

    All exercise tables up and down script implemented

---
+++ Powerlevel/Powerlevel/Powerlevel/App_Data/EXERCISE_DOWN.sql
@@ -0,0 +1,14 @@
+ALTER TABLE	 [dbo].[ExerciseFlags]		DROP CONSTRAINT [FK_dbo.Exercises_Flags]
+ALTER TABLE	 [dbo].[ExerciseEquipment]	DROP CONSTRAINT [FK_dbo.Exercises_Equipment]
+ALTER TABLE  [dbo].[ExerciseImages] 	DROP CONSTRAINT [FK_dbo.Exercises_Images]
+ALTER TABLE  [dbo].[Exercises] 			DROP CONSTRAINT [PK_dbo.Exercises]
+ALTER TABLE	 [dbo].[ExerciseEquipment]	DROP CONSTRAINT [PK_dbo.ExerciseEquipment]
+ALTER TABLE  [dbo].[ExerciseFlags] 		DROP CONSTRAINT [PK_dbo.ExerciseFlags]
+ALTER TABLE  [dbo].[ExerciseImages] 	DROP CONSTRAINT [PK_dbo.ExerciseImages]
+
+DROP TABLE [dbo].[Exercises] 	
+DROP TABLE [dbo].[ExerciseFlags]
+DROP TABLE [dbo].[ExerciseEquipment]
+DROP TABLE [dbo].[ExerciseImages] 
+
+GO
---
+++ Powerlevel/Powerlevel/Powerlevel/App_Data/EXERCISE_LIST_UP.sql
@@ -0,0 +1,2 @@
+/* This file is only for populating the lists of exercises, as such a list is exhaustive */
+
