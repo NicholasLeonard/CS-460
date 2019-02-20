ef2df8d3f1c5e244b435900d4483620b90ba29f4
Jace Woods <woodsjace@yahoo.com>
2019-02-19 19:02:17 -0800

    exercise seeding updated with more exercises

--- Powerlevel/Powerlevel/Powerlevel/App_Data/WORKOUT_UP.sql
+++ Powerlevel/Powerlevel/Powerlevel/App_Data/WORKOUT_UP.sql
@@ -27,20 +27,4 @@ CREATE TABLE [dbo].[WorkoutExercises]
 		ON UPDATE CASCADE
 );
 
-/* Create the table that contains various workouts, whether they are strength or cardio,
-the muscle group that it focuses on, and approximately how long the full workout takes to complete */
-INSERT INTO [dbo].[Workouts] (Name, Type, MainMuscleFocus, TimeEstimate) VALUES
-('Bench Press', 'Strength', 'Chest', '8 Minutes'),
-('Bent-over row', 'Strength', 'Back', '12.5 Minutes'),
-('Chin-up', 'Strength', 'Back', '2 Minutes'),
-('Incline flye', 'Strength', 'Chest', '2 Minutes'),
-('Diamond push-up', 'Strength', 'Chest', '4.5 Minutes'),
-('Dumbbell overhead press', 'Strength', 'Anterior Deltoids', '10.5 Minutes'),
-('Hammer-grip dumbbell bench press', 'Strength', 'Chest', '1.5 Minutes'),
-('Dumbbell triceps extension', 'Strength', 'Triceps', '1.5 Minutes')
-
-/* Links each workout exercise to a workout, an exercise, and an order to complete the workout */
-INSERT INTO [dbo].[WorkoutExercises] (WorkoutId, ExerciseId, OrderNumber) VALUES
-(0,0,1)
-
 
