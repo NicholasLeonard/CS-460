018130112f7c10b9c1b3e3bff9f2e61d70a77464
Alex-Bishop1296 <alex.bishop1296@gmail.com>
2019-02-23 15:11:00 -0800

    cut out uneeded exercise CRUD pages and added display images for exercises

--- Powerlevel/Powerlevel/Powerlevel/Controllers/ExercisController.cs
+++ Powerlevel/Powerlevel/Powerlevel/Controllers/ExercisController.cs
@@ -28,6 +28,7 @@ namespace Powerlevel.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Exercis exercis = db.Exercises.Find(id);
+            ViewBag.Images = db.ExerciseImages.Where(x => x.ExerciseId == exercis.ExerciseId).ToList();
             if (exercis == null)
             {
                 return HttpNotFound();
