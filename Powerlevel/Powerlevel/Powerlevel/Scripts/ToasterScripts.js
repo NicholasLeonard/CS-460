2833471ccd8df3165051882e3f3a9db9ab5e30f6
NicholasLeonard <n.leonard1@outlook.com>
2019-04-10 14:40:55 -0700

    Implemented calendar on main page.

--- Powerlevel/Powerlevel/Powerlevel/Scripts/ToasterScripts.js
+++ Powerlevel/Powerlevel/Powerlevel/Scripts/ToasterScripts.js
@@ -1,7 +1,16 @@
-﻿
-$(document).ready(function () {
+﻿$(document).ready(function () {
     //Table here is created on Exercise/Index to generate all exercise entries
     $('#myTable').DataTable({
         paging: false
     });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarE1 = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarE1, {
+        plugins: ['dayGrid']
+    });
+
+    calendar.render();
 });
