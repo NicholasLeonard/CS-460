1d9e82b6edc945e0fd87cbbf7b3b77bf41ad593e
NicholasLeonard <n.leonard1@outlook.com>
2019-04-27 12:36:22 -0700

    Finished integrating Chi's stuff

--- Powerlevel/Powerlevel/Powerlevel/App_Data/DOWN/USER_WORKOUT_DOWN.sql
+++ Powerlevel/Powerlevel/Powerlevel/App_Data/DOWN/USER_WORKOUT_DOWN.sql
@@ -1,4 +1,12 @@
-﻿/* DROP all User Workout tables */
-DROP TABLE IF EXISTS [dbo].[UserWorkout] 
+﻿/*Drop Primary key*/
+ALTER TABLE [dbo].[UserWorkout] DROP CONSTRAINT [PK_dbo.UserWorkout]
+GO
 
+/*Drop Foreign keys*/
+ALTER TABLE [dbo].[UserWorkout] DROP CONSTRAINT [FK_dbo.UserWorkout_User]
+ALTER TABLE [dbo].[UserWorkout] DROP CONSTRAINT [FK_dbo.UserWorkout_Workout]
+GO
+
+/*Drop Table*/
+DROP TABLE IF EXISTS [dbo].[UserWorkout] 
 GO
