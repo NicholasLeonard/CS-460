8e98a6929ef4abd141de66dda7a6f7117695c5e0
NicholasLeonard <n.leonard1@outlook.com>
2020-03-11 11:41:29 -0700

    Completed 'More on Styled Components' tutorial.

--- Udemy/React/LectureResources/styling-react-components/src/App.js
+++ Udemy/React/LectureResources/styling-react-components/src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import Styled from 'styled-components';
+import styled from 'styled-components';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: green;
+  font: inherit;
+  color: white;
+  padding: 8px;
+  border: 1px solid blue;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: lightgreen;
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -94,9 +108,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <StyledButton
+          onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
         {persons}
       </div>
     );
--- Udemy/React/LectureResources/styling-react-components/src/Person/Person.js
+++ Udemy/React/LectureResources/styling-react-components/src/Person/Person.js
@@ -16,11 +16,6 @@ width: 450px;
 }`;
 
 const person = (props) => {
-  const style = {
-    '@media (min-width: 500px)': {
-      width: '450px'
-    }
-  };
   return (
     // <div className="Person" style={style}>
     <StyledDiv>
