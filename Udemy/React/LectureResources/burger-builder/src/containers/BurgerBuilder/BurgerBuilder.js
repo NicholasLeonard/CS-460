fdbf62e7164b5b5d7c4305c38269b5ea9b041e9f
NicholasLeonard <n.leonard1@outlook.com>
2020-03-18 11:36:07 -0700

    Completed 'Creating a Layout Component and Starting Implementation of The Burger Builder Container' tutorials.

---
+++ Udemy/React/LectureResources/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxilary';
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burger</div>
+        <div>Build Controlsw</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
