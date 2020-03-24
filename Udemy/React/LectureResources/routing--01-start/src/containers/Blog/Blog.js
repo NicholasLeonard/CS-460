d023b3a70324b1aae78cb494a2733a71d02c739e
NicholasLeonard <n.leonard1@outlook.com>
2020-03-24 15:12:18 -0700

    Completed 'Understanding Nested Routes' tutorial.

--- Udemy/React/LectureResources/routing--01-start/src/containers/Blog/Blog.js
+++ Udemy/React/LectureResources/routing--01-start/src/containers/Blog/Blog.js
@@ -5,25 +5,22 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
-
   render() {
-
     return (
       <div className="Blog">
         <header>
           <nav>
             <ul>
               <li><NavLink
-                to="/"
+                to="/posts/"
                 exact
                 activeClassName="my-active"
                 activeStyle={{
                   color: '#fa923f',
                   textDecoration: 'underline'
-                }}>Home</NavLink></li>
+                }}>Posts</NavLink></li>
               <li><NavLink to={{
                 pathname: '/new-post',
                 hash: '#submit',
@@ -33,11 +30,10 @@ class Blog extends Component {
           </nav>
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
-        <Route path="/" exact render={() => <h1>Home 2</h1>} /> */}
-        <Route path="/" exact component={Posts} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Switch>
           <Route path="/new-post" component={NewPost} />
-          <Route path="/:id" exact component={FullPost} />
+          <Route path="/posts" component={Posts} />
         </Switch>
       </div>
     );
