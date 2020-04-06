d35c6f12dd000f27963e8e79b9e03be26dfc868d
NicholasLeonard <n.leonard1@outlook.com>
2020-04-06 09:40:40 -0700

    Completed 'Fetching Orders (via Redux)' tutorial for Burger Builder.

--- Udemy/React/LectureResources/burger-builder/src/containers/Checkout/Checkout.js
+++ Udemy/React/LectureResources/burger-builder/src/containers/Checkout/Checkout.js
@@ -4,12 +4,9 @@ import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from '../Checkout/ContactData/ContactData';
+import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
-
-  componentWillMount() {
-    this.props.purchaseInit();
-  }
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
