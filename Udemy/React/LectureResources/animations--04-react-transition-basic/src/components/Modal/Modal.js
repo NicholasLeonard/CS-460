bf4f43f2639d2a921baf63d8a60a4c26621b3428
NicholasLeonard <n.leonard1@outlook.com>
2020-04-08 11:49:42 -0700

    Completed 'Transition Events' tutorial.

--- Udemy/React/LectureResources/animations--04-react-transition-basic/src/components/Modal/Modal.js
+++ Udemy/React/LectureResources/animations--04-react-transition-basic/src/components/Modal/Modal.js
@@ -10,7 +10,18 @@ const animationTiming = {
 
 const modal = props => {
   return (
-    <Transition mountOnEnter unmountOnExit in={props.show} timeout={animationTiming}>
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+      onEnter={() => console.log('onEnter')}
+      onEntering={() => console.log('onEntering')}
+      onEntered={() => console.log('onEntered')}
+      onExit={() => console.log('onExit')}
+      onExiting={() => console.log('onExiting')}
+      onExited={() => console.log('onExited')}
+    >
       {state => {
         const cssClasses = [
           "Modal",
